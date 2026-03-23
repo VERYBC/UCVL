23fb5c9d113c2940c20f0816392a424e55c58ea5
verybc <150411573+VERYBC@users.noreply.github.com>
2026-03-23 11:54:48 +0800

    Add files via upload

---
+++ feature_match/Romav2/device.py
@@ -0,0 +1,11 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "2" 
+import torch
+
+device = torch.device("cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
